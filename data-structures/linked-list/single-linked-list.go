7e5af321158fc4cea78b49f3e3a4f6ac72a64053
chetannn <chetankharel7@gmail.com>
2022-09-04 16:29:25 +0545

    wip

--- data-structures/linked-list/single-linked-list.go
+++ data-structures/linked-list/single-linked-list.go
@@ -50,6 +50,15 @@ func (list *LinkedList) AddLast(data int) {
 
 }
 
+func (list *LinkedList) DeleteFirst() {
+
+  if list.size == 1 {
+    list.head = nil
+  }
+
+    list.head = list.head.next
+}
+
 func (list *LinkedList) Reverse() {
 
   current := list.head
@@ -68,12 +77,88 @@ func (list *LinkedList) Reverse() {
   list.head = prev
 
 }
-func (list *LinkedList) Display() { current := list.head
+
+func (list *LinkedList) GetAt(index int) *Node {
+
+  if index > 0 && index > list.size {
+    panic("Index out of bound")
+  }
+  
+  current := list.head
+
+  for i := 0; i < index; i++ {
+    current = current.next
+  }
+
+  return current
+
+}
+
+func (list *LinkedList) InsertAt(index int, data int) {
+
+  if index > list.size {
+    panic("Index out of bound")
+  }
+
+  if index == 0 {
+    list.AddFirst(data)
+  }
+
+  if index == list.size - 1 {
+      list.AddLast(data)
+    } else {
+    prevNode := list.GetAt(index - 1)
+    newNode := &Node { data: data, next: nil }
+  
+    newNode.next = prevNode.next.next
+    prevNode.next = newNode
+    list.size++
+
+    }
+
+}
+
+func (list *LinkedList) DeleteAt(index int) {
+
+
+  if index > list.size {
+    panic("Index out of bound")
+  }
+
+  if index == 0 {
+    list.DeleteFirst()
+  }
+
+
+  if index == list.size - 1 {
+
+    current := list.head
+
+    for current.next != nil {
+      current = current.next
+    }
+
+    current = nil
+
+  } else {
+    prevNode := list.GetAt(index - 1)
+    prevNode.next = prevNode.next.next
+  }
+
+  list.size--
+    
+}
+
+func (list *LinkedList) Display() { 
+
+  current := list.head
 
   for current != nil {
      fmt.Printf("%v->", current.data)
     current = current.next
   }
+
+  fmt.Println()
 }
 
 func main() {
@@ -89,6 +174,18 @@ func main() {
   fmt.Println("\n")
 
   list.Reverse()
+
+  list.Display()
+
+  node := list.GetAt(2)
+
+  fmt.Println("Node at index 2:", node)
+
+  list.DeleteAt(2)
+
+  list.Display()
+
+  list.InsertAt(2, 5)
 
   list.Display()
 
