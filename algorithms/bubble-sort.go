ec0e2894151611fbdea654a5a3ee33f0f6ecb58c
chetannn <chetankharel7@gmail.com>
2022-09-05 19:06:46 +0545

    wip

--- algorithms/bubble-sort.go
+++ algorithms/bubble-sort.go
@@ -7,18 +7,37 @@ func main() {
   arr := []int{5,3,2,1,3,5,6}
   temp := 0
 
-  for i := 0; i < len(arr); i++ {
-    for j :=i+1; j < len(arr); j++ {
+  bubbleSort(arr, temp)
 
-      if arr[j] < arr[i] {
-          temp = arr[i]
-          arr[i] = arr[j]
-          arr[j] = temp
+  fmt.Println("Sorted arr:", arr)
+
+}
+
+
+func bubbleSort(arr []int, temp int) {
+
+   isSorted := false
+
+    for i := 0; i < len(arr); i++ {
+
+      isSorted = true
+
+    for j :=1; j < len(arr) - i; j++ {
+
+      if arr[j] < arr[j - 1] {
+          temp = arr[j]
+          arr[j] = arr[j-1]
+          arr[j-1] = temp
+
+          isSorted = false
       }
-    }  
+
+      if isSorted {
+        return
+      }
+    }
 
   }
 
-  fmt.Println("Sorted arr:", arr)
 
 }
